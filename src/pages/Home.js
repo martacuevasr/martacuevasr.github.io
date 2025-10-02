5d96d73a2906b945a1994bf753188e5387edbbb1
Marta Cuevas <marcuerodri@gmail.com>
2025-10-02 19:57:39 +0200

    Update image source to use process.env.PUBLIC_URL

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -9,7 +9,7 @@ const Home = () => {
                 <div className="home-image">
                     <div className={styles.profileImage}>
                         <img 
-                            src="%PUBLIC_URL%/memoji.png" 
+                            src="process.env.PUBLIC_URL/memoji.png" 
                             alt="Marta Cuevas - Memoji" 
                             className={styles.memojiImage}
                         />
